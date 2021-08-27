9f1521171e25ec52ac8353a0d6c077e538c40814
JaeYoonKimme <jeyun0219@naver.com>
2021-08-27 12:54:37 +0900

    clean dummies

--- Chap8/strings/target/debug/deps/strings.d
+++
@@ -1,5 +0,0 @@
-/Users/yoon/Desktop/Yoon/Rust_Programming_Language/Chap8/strings/target/debug/deps/strings: src/main.rs
-
-/Users/yoon/Desktop/Yoon/Rust_Programming_Language/Chap8/strings/target/debug/deps/strings.d: src/main.rs
-
-src/main.rs:
--- Chap8/strings/target/debug/strings.d
+++
@@ -1,1 +0,0 @@
-/Users/yoon/Desktop/Yoon/Rust_Programming_Language/Chap8/strings/target/debug/strings: /Users/yoon/Desktop/Yoon/Rust_Programming_Language/Chap8/strings/src/main.rs
