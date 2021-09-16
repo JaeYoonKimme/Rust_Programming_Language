20084328511d049c61910b2635da5b0b255f6473
JaeYoonKimme <jeyun0219@naver.com>
2021-09-16 21:09:56 +0900

    buf fix

--- Chap12/minigrep/src/main.rs
+++ Chap12/minigrep/src/main.rs
@@ -4,9 +4,9 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-	let args: Vec<String> = env::args().collect();
-
 	let config = Config::new(&args).unwrap_or_else(|err| {
+			let args: Vec<String> = eng::args().collect();
+
 			eprintln!("Problem parsing arguments: {}",err);
 			process::exit(1);
 			});
